51034b20635988dca234c84bd54401e70c3affe8
dianlujitao <dianlujitao@lineageos.org>
2020-03-04 00:16:48 +0800

    lavender: Switch to qti vibrator HAL
    Change-Id: I2ac3fb7ab15eb316999427697d97dd403d5c7ef1
    
    lavender: nuke vibrator 1.0 from manifest
    
    Signed-off-by: alanndz <alanmahmud0@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- device.mk
+++ device.mk
@@ -96,8 +96,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-	android.hardware.vibrator@1.0-impl \
-	android.hardware.vibrator@1.0-service
+	vendor.qti.hardware.vibrator@1.2-service
 
 # Vendor files
 $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)

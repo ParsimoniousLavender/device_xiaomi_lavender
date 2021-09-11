4a8380a1a5223b1c60b9dd6ab08bbf945b032f49
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-11 18:23:20 +0200

    lavender: Drop product_launched_with_*.mk
    * It seems Google stopped making those
    * Directly set PRODUCT_SHIPPING_API_LEVEL
    
    Change-Id: If00badd6dc12e0245c74bba12e4816361091e754
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 	 $(DEVICE_PATH)/overlay \
 	 $(DEVICE_PATH)/overlay-lineage
 
+# PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
+PRODUCT_SHIPPING_API_LEVEL := 28
+
 # Audio
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \

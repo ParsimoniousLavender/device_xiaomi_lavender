5cf6d0881784a998e54ffd1e8a7e655409bf0730
pix106 <sbordenave@gmail.com>
2022-10-04 20:02:53 +0200

    lavender: Enable APEX device-side
    This reverts commit e540af1b602e3adcd19cf01524be47949e714934.

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 # PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 28
 
+# APEX
+ENABLE_APEX := true
+
 # Audio
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
